a3cd7c0a3880d14e24a8489645d0c81390e0b837
Rushikesh Bhomale <rushikeshbhomale@gmai.com>
2022-12-25 00:36:32 +0530

    problem1

---
+++ Introduction_JS/problem1.js
@@ -0,0 +1,10 @@
+console.log("Masai School");
+
+console.log("A Transformation in Education");
+
+//two methods for answere 
+
+let a= "Masai School";
+console.log(a);
+let b= "A Transformation in Education";
+console.log(b);
