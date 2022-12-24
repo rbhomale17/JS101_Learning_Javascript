40cd59a48dd40a0f342fc8e721e968db3dae978a
Rushikesh Bhomale <rushikeshbhomale@gmai.com>
2022-12-25 00:53:04 +0530

    problem4

---
+++ Introduction_JS/problem4.js
@@ -0,0 +1,24 @@
+let a= "---------------------------------------"
+console.log(a);
+let heading= "******** 🅡🅤🅢🅤🅛🅣 ********";
+let name= "Rushikesh Diliprao Bhomale";
+let school= "Masai School";
+let grade= "1st Unit";
+let section="A";
+let rollno= "fw25_348";
+let English= 62;
+let Science= 56;
+let Math= 87;
+console.log(heading);
+console.log("Name -",name);
+console.log("School -",school);
+console.log("Grade -",grade);
+console.log("Section -",section);
+console.log("Rollno -",rollno);
+console.log("---------------------------------------")
+console.log("Statement of Marks");
+console.log("English -", English);
+console.log("Science -", Science);
+console.log("Math-", Math);
+console.log("---------------------------------------")
+console.log("Total Marks =",English+Science+Math)
